894777c684482b61ef245ed5f0a6618e6ffe9cc2
Sydney Jones <sydneyjones87@gmail.com>
2022-12-14 18:15:14 -0600

    mvc

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit, Input } from "@angular/core";
+import { Satellite } from "../satellite";
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+  selector: "app-orbit-counts",
+  templateUrl: "./orbit-counts.component.html",
+  styleUrls: ["./orbit-counts.component.css"],
 })
 export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
 
-	@Input() satellites: Satellite[];
+  // added listCount property to store length of satellite array
+  listCount: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.listCount = this.satellites.length;
   }
 
+  ngOnInit() {}
+
   countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
-	}
-	return count;
- }
-
-
+    let count = 0;
+    if (this.satellites) {
+      for (let i = 0; i < this.satellites.length; i++) {
+        if (this.satellites[i].type === type) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
 }
